8cf7908ccad022fce6ac206fba25365faac0b076
MarynaPotiievska <101947859+MarynaPotiievska@users.noreply.github.com>
2023-05-19 12:30:43 +0300

    added option extended: true

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
+const authRouter = require("./routes/api/auth");
+const noticesRouter = require("./routes/api/notices");
+const petsRouter = require("./routes/api/pets");
+const userRouter = require("./routes/api/user");
 
-const authRouter = require('./routes/api/auth')
-const noticesRouter = require("./routes/api/notices")
-const petsRouter = require('./routes/api/pets')
-const userRouter = require('./routes/api/user')
+const app = express();
 
-const app = express()
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/auth', authRouter)
-app.use('/api/notices', noticesRouter)
-app.use('/api/pets', petsRouter)
-app.use('/api/user', userRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/notices", noticesRouter);
+app.use("/api/pets", petsRouter);
+app.use("/api/user", userRouter);
 
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
-})
+  res.status(404).json({ message: "Not found" });
+});
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
-})
+});
 
-module.exports = app
+module.exports = app;
