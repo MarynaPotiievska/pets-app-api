5f041a3ee072a806652bbd0fb7f7968e97656885
VikaVelhan <velganvika@gmail.com>
2023-05-31 20:52:01 +0300

    update RegExp

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -3,15 +3,22 @@ const { validationResult } = require("express-validator");
 
 const validateBody = (validations) => {
   return async (req, res, next) => {
-    for (const validation of validations) {
-      const result = await validation.run(req);
-      if (result.errors.length) break;
+    try {
+      for (const validation of validations) {
+        const result = await validation.run(req);
+        if (result.errors.length) break;
+      }
+      const errors = validationResult(req);
+      if (errors.isEmpty()) {
+        return next();
+      }
+      const errorMessages = errors.errors.map(
+        (error) => `${error.msg} in ${error.param}`
+      );
+      next(new HttpError(400, errorMessages));
+    } catch (error) {
+      next(error);
     }
-    const errors = validationResult(req);
-    if (errors.isEmpty()) {
-      return next();
-    }
-    next(HttpError(400, `${errors.errors[0].msg} in ${errors.errors[0].path}`));
   };
 };
 
