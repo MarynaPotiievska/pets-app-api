e02fec703301d8c710c271c4e2b90dc3f1c8bc10
VikaVelhan <velganvika@gmail.com>
2023-05-31 23:34:33 +0300

    Update RegExp.js

--- constants/RegExp.js
+++ constants/RegExp.js
@@ -3,7 +3,7 @@ const datePattern = /^\d{2}.\d{2}.\d{4}$/;
 const breedPattern = /^[a-zA-Z ]+$/;
 const emailPattern = /^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/;
 const phonePattern = /^[+]380?[-\s]?([5|6|9][0|3|5|6|8|9])?[-.\s]?[0-9]{7}$/;
-const passwordPattern = /^[a-zA-Z0-9]+$/;
+const passwordPattern = /^[a-zA-Z0-9[:punct:]]+$/;
 const locationPattern = /^[A-Za-z\s]+$/;
 
 module.exports = {
