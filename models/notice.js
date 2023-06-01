9b8798b2fa119fd540a4745a270835c37a0c061e
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-06-01 16:10:31 +0200

    update model noties

--- models/notice.js
+++ models/notice.js
@@ -1,8 +1,12 @@
 const { Schema, model } = require("mongoose");
 const { body } = require("express-validator");
-
-const dateRegexp = /^\d{2}([.])\d{2}([.])\d{4}$/;
-const locationRegex = /^[a-zA-Z ]+$/;
+const { validateData } = require("../middlewares");
+const {
+  namePattern,
+  datePattern,
+  breedPattern,
+  locationPattern,
+} = require("../constants/RegExp");
 
 const noticeSchema = new Schema(
   {
@@ -17,17 +21,28 @@ const noticeSchema = new Schema(
     },
     name: {
       type: String,
+      validate: {
+        validator: validateData.validateName,
+        message: "Name is invalid",
+      },
       required: [true, "Name is required"],
       minlength: 2,
       maxlength: 16,
     },
     date: {
       type: String,
-      match: dateRegexp,
+      validate: {
+        validator: validateData.validateDate,
+        message: "Date of birth is invalid",
+      },
       required: [true, "Date is required"],
     },
     breed: {
       type: String,
+      validate: {
+        validator: validateData.validateBreed,
+        message: "Breed is invalid",
+      },
       required: [true, "Breed is required"],
       minlength: 2,
       maxlength: 16,
@@ -44,15 +59,22 @@ const noticeSchema = new Schema(
     },
     location: {
       type: String,
+      validate: {
+        validator: validateData.validateLocatione,
+        message: 'Location should be in the format of "City".',
+      },
       required: true,
-      match: [locationRegex, 'Location should be in the format of "City".'],
     },
     price: {
       type: Number,
       min: [1, "Price must be higher than 0"],
       validate: {
         validator: function (value) {
-          return this.category !== "lost-found" && this.category !== "for-free" && value !== null;
+          return (
+            this.category !== "lost-found" &&
+            this.category !== "for-free" &&
+            value !== null
+          );
         },
         message: "Price is not allowed for this category",
       },
@@ -77,16 +99,24 @@ const noticeSchema = new Schema(
 
 const schema = [
   body("title").isString().notEmpty(),
-  body("name").isString().notEmpty().isLength({ min: 2, max: 16 }),
-  body("date").isString().notEmpty().matches(dateRegexp),
-  body("breed").isString().notEmpty().isLength({ min: 2, max: 16 }),
+  body("name")
+    .isString()
+    .notEmpty()
+    .matches(namePattern)
+    .isLength({ min: 2, max: 16 }),
+  body("date").isString().notEmpty().matches(datePattern),
+  body("breed")
+    .isString()
+    .notEmpty()
+    .matches(breedPattern)
+    .isLength({ min: 2, max: 16 }),
   body("category").isIn(["sell", "lost-found", "for-free"]).notEmpty(),
   body("sex").isString().notEmpty().isIn(["male", "female"]),
   body("comments").isString().isLength({ min: 8, max: 120 }),
   body("location")
     .isString()
     .notEmpty()
-    .matches(locationRegex)
+    .matches(locationPattern)
     .withMessage('Location should be in the format of "City".'),
   body("price")
     .if(body("category").exists().equals("sell"))
