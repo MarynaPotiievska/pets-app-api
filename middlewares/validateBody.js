c514aefc43d08e4dc1093c7eb1152f958d91f3f3
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-06-01 07:15:45 +0200

    update

--- middlewares/validateBody.js
+++ middlewares/validateBody.js
@@ -4,18 +4,28 @@ const { validationResult } = require("express-validator");
 const validateBody = (validations) => {
   return async (req, res, next) => {
     try {
-      for (const validation of validations) {
-        const result = await validation.run(req);
-        if (result.errors.length) break;
-      }
+       await Promise.all(validations.map((validation) => validation.run(req)));
       const errors = validationResult(req);
+
       if (errors.isEmpty()) {
         return next();
       }
-      
-      const errorMessages = errors.errors.map((error) => `${error.msg} in field ${error.path}`);
-      console.log(errors.errors)
-      next(HttpError(400, errorMessages));
+
+      const errorFields = [];
+      const errorMessages = [];
+
+      errors.array().forEach((error) => {
+        if (!errorFields.includes(error.path)) {
+          errorFields.push(error.path);
+        }
+        errorMessages.push(`${error.msg} in field ${error.path}`);
+      });
+
+      const uniqueErrorMessages = errorFields.map((field) =>
+        errorMessages.filter((msg) => msg.includes(`in field ${field}`)).join(", ")
+      );
+
+      next(HttpError(400, uniqueErrorMessages));
     } catch (error) {
       next(error);
     }
